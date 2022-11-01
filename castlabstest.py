fd387153093e3d89a6498966ccc32557a8ebddcb
Sergei <60666684+sambelony@users.noreply.github.com>
2022-11-02 00:58:13 +0200

    Add files via upload

--- castlabstest.py
+++ castlabstest.py
@@ -5,11 +5,6 @@ from selenium.webdriver import ActionChains, DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service
-
-#Approach without setting up logs
-# s = Service("C:/Users/grayb/Desktop/castlabs/chromedriver.exe")
-# driver = webdriver.Chrome(service=s)
 
 #Setting up logs and driver
 d = DesiredCapabilities.CHROME
@@ -21,74 +16,73 @@ driver = webdriver.Chrome('C:/Users/grayb/Desktop/castlabs/chromedriver.exe', de
 """
 driver.get("https://demo.castlabs.com/")
 
-try:
-    """
-    2) Play 'HLS - HLS Clear' by clicking on the stream.
-    3) The browser opens with the details of the video.
-    """
-    hls_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//li[text()='HLS']")))
-    hls_button.click()
+"""
+2) Play 'HLS - HLS Clear' by clicking on the stream.
+3) The browser opens with the details of the video.
+"""
+hls_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//li[text()='HLS']")))
+hls_button.click()
 
-    """
-    4)Seek to 70% of the video duration.
-    """
-    #Sleep before player will start playing
-    time.sleep(1)
+"""
+4) Seek to 70% of the video duration.
+"""
+#Sleep before player will start playing
+time.sleep(1)
 
-    #Clicking on player to reveal the timeline
-    player = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "player-container")))
-    player.click()
+#Clicking on player to reveal the timeline
+player = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "player-container")))
+player.click()
 
-    #Finding the timeline and slider
-    timeline = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "pp-ui-clickable")))
-    slider = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[5]/div["
-                                                  "3]/div/div[1]/div/div[4]")))
+#Finding the timeline and slider
+timeline = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "pp-ui-clickable")))
+slider = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[5]/div["
+                                              "3]/div/div[1]/div/div[4]")))
 
-    #Getting the slider width and performing drag and drop slider from the starting position to 70% (0,7 of width)
-    ActionChains(driver).drag_and_drop_by_offset(slider, 0.7 * timeline.size['width'], 0).perform()
 
-    """
-    5) Validate the "Player state Change" in the console logs.
-    """
-    for logline in driver.get_log('browser'):
-        if "seeking" in str(logline):
-            print("Seeking confirmed")
+#Getting the slider width and performing drag and drop slider from the starting position to 70% (0,7 of width)
+ActionChains(driver).drag_and_drop_by_offset(slider, 0.7 * timeline.size['width'], 0).perform()
 
-    """
-    6) Click the option button (three dots), click 'Videos'.
-    """
-    #Clicking on option button
-    options = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[5]/div["
-                                                  "1]/button[4]")))
-    options.click()
+"""
+5) Validate the "Player state Change" in the console logs.
+"""
+assert "seeking" in str(driver.get_log('browser'))
 
-    #Clicking on 'Videos'
-    video = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[4]/div/div["
-                                                  "2]/div[3]/div[1]")))
-    video.click()
+"""
+6) Click the option button (three dots), click 'Videos'.
+"""
+#Clicking on option button
+options = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[5]/div["
+                                              "1]/button[4]")))
+options.click()
 
-    """
-    7) Select any video value eg: '960x540 @ 2.48 Mbps' and Validate the selection.
-    """
-    #Sleep before the list unfolds
-    time.sleep(1)
+#Clicking on 'Videos'
+video = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[4]/div/div["
+                                              "2]/div[3]/div[1]")))
+video.click()
 
-    #Selecting 960x540 @ 2.48 Mbps
-    video_value = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located(
-            (By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[4]/div/div[2]/div[3]/div[2]/div[4]")))
-    video_value.click()
+"""
+7) Select any video value eg: '960x540 @ 2.48 Mbps' and Validate the selection.
+"""
+#Sleep before the list unfolds
+time.sleep(1)
 
-    #Selection validating:
-    for logline in driver.get_log('browser'):
-        if "ratechange" in str(logline):
-            print("Ratechange confirmed")
+#Selecting 960x540 @ 2.48 Mbps
+video_value = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located(
+        (By.XPATH, "/html/body/div[2]/div[2]/div/div[1]/div[1]/div/div[4]/div/div[2]/div[3]/div[2]/div[4]")))
+video_value.click()
 
-except:
-    driver.quit()
+#Selection validating:
+assert "ratechange" in str(driver.get_log('browser'))
+
+print("Passed!")
+
+driver.quit()
+
+
 
